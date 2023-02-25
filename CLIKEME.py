bd522ac2364ef4a815c7876bfeae7bf47405c4e1
LiHao <125774778+Explorers8741@users.noreply.github.com>
2023-02-25 11:34:41 +0800

    Update and rename README.md to CLIKEME.py

---
+++ CLIKEME.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+#This programs can run in Python
+print("WARNING! This programs is copyrighted by Explorers8741!")
+input("Press enter to continue...")
+a=input("Do you want to kown me? (yes or no):")
+if a==yes:
+    input("Yes! Press enter to continue...")
+if a!=yes:
+    a=input('ERROR! You must input "yes"!')
+print("
