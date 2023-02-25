81f7a1a3dccedc86b73983c18386698d17ceac64
LiHao <125774778+Explorers8741@users.noreply.github.com>
2023-02-25 12:02:16 +0800

    Update CLIKEME.py

--- CLIKEME.py
+++ CLIKEME.py
@@ -7,4 +7,14 @@ if a==yes:
     input("Yes! Press enter to continue...")
 if a!=yes:
     a=input('ERROR! You must input "yes"!')
-print("
+print("Hello, my name is Li Hao .")
+print("My nickname is Explorers8741")
+print("I'm from China! I'm a Chinese!")
+print("I like coding.")
+print("Maybe we can be a good friend!")
+print("You can find me here")
+input("Press enter to continue...")
+print("E-Mail: RF-Explorers@outlook.com")
+print("QQ: 210055329")
+print("All right.Let's end it now.")
+input("Press enter to exit...")
